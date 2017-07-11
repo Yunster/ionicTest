4147ee4d200c0fad01014755626f09a901a93bfe
Yunster <kysung1234@naver.com>
2017-07-11 17:25:02 +0900

    testApp First

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class Profile {
+    
+  constructor(public alertCtrl: AlertController) {
 
-  constructor(public navCtrl: NavController) {
-
+  }
+  
+  fndSave(){
+      
+      let isDone: boolean = false;
+      
+      let alert = this.alertCtrl.create({
+          title: 'Information',
+          subTitle: "kkkk",
+          buttons: ['OK']
+      });
+      
+      alert.present();
   }
 
 }
