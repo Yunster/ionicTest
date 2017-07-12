ff015cd1e84f524b1e0ebad8a6701844c515e094
Yunster <kysung1234@naver.com>
2017-07-12 17:56:00 +0900

    Test_class

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,18 +13,38 @@ export class Profile {
   
   fndSave(){
       
-      let decimal: number = 6;
-      let hex: number = 0xf00d;
-      let binary: number = 0b1010;
-      let octal: number = 0o744;
+      // Type test
+      let todayMenu = new Menu(['pancakes', 'waffles', 'orange juice'], 1);
       
       let alert = this.alertCtrl.create({
           title: 'Information',
-          subTitle: decimal + ", " + hex + ", " + binary + ", " + octal,
+          subTitle: todayMenu.list(),
           buttons: ['OK']
       });
       
       alert.present();
   }
 
+}
+
+class Menu {
+    items: Array<string>;
+    pages: number;
+    
+    constructor(item_list: Array<string>, total_pages: number) {
+        this.items = item_list;
+        this.pages = total_pages;
+    }
+    
+    list(): string {
+        let msg = 'Our menu for today : ';
+        for(let i=0, len=this.items.length ; i<len ; i++) {
+            msg += this.items[i];
+            if(i != len - 1) {
+                msg += ", ";
+            }
+        }
+        
+        return msg;
+    }
 }
